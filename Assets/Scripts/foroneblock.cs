467b02dc2cfc368c40724099cfb82e53cae4ff44
henrydai1995 <37208339+henrydai1995@users.noreply.github.com>
2020-07-28 20:25:47 +0800

    Add files via upload

---
+++ Assets/Scripts/foroneblock.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.UIWidgets.widgets;
+using UnityEditor;
+using UnityEngine;
+
+public class foroneblock : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public Vector3 rotationPoint;
+    public static int height = 24;
+    public static int width = 10;
+    public static UnityEngine.Transform[,] bitmap = new UnityEngine.Transform[height, width];
+    public GameObject startLocation;
+    public bool gameOver;
+    private float pretime;
+    public float falltime = 0.8f;
+    void Start()
+    {
+        pretime = Time.time;
+        gameOver = false;
+        startLocation = GameObject.FindGameObjectWithTag("StartLocation");
+        if (!moveValid())
+        {
+            gameOver = true;
+            startLocation.SendMessage("setGame");
+        }
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!gameOver)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                transform.position += new Vector3(-1,0,0);
+                if(!moveValid())
+                    transform.position -= new Vector3(-1, 0, 0);
+            }
+            if(Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                transform.position += new Vector3(1, 0, 0);
+                if(!moveValid())
+                    transform.position -= new Vector3(1, 0, 0);
+            }
+            if(Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                transform.RotateAround(transform.TransformPoint(Vector3.zero),Vector3.forward,-90);
+                if(!moveValid())
+                    transform.RotateAround(transform.TransformPoint(Vector3.zero), Vector3.forward, 90);
+            }
+
+            if(Input.GetKeyDown(KeyCode.DownArrow) || (Time.time-pretime>falltime))
+            {
+                transform.position += new Vector3(0,-1,0);
+                if(!moveValid())
+                {
+                    transform.position -= new Vector3(0, -1, 0);
+                    addbit();
+                    checkFullLine();
+                    startLocation.SendMessage("newAblock");
+                    this.enabled = false;
+                }
+                pretime = Time.time;
+            }
+        }
+    }
+    
+    void checkFullLine()
+    {
+        for(int row = 0;row<height;++row)
+        {
+            if(checkLine(row))
+            {
+                startLocation.SendMessage("addGrade");
+                deleteLine(row);
+                moveLines(row);
+                --row;
+            }
+        }
+    }
+    void moveLines(int row)
+    {
+        for(;row<height-1;++row)
+        {
+            for(int col=0;col<width;++col)
+            {
+                if (bitmap[row+1, col] != null)
+                {
+                    bitmap[row, col] = bitmap[row + 1, col];
+                    bitmap[row, col].transform.position += new Vector3(0,-1,0);
+                    bitmap[row + 1, col] = null;
+                }
+                
+            }
+        }
+    }
+    void deleteLine(int row)
+    {
+        for(int col=0;col<width;++col)
+        {
+            Destroy(bitmap[row,col].gameObject);
+            bitmap[row, col] = null;
+        }
+    }
+    bool checkLine(int row)
+    {
+        for(int col=0;col<width;++col)
+        {
+            if (bitmap[row, col] == null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    void addbit()
+    {
+        foreach (UnityEngine.Transform child in transform)
+        {
+            //int x = Mathf.RoundToInt(child.transform.position.x);
+            int col = Mathf.RoundToInt(child.transform.position.x);
+            int row = Mathf.RoundToInt(child.transform.position.y);
+            bitmap[row, col] = child;
+        }
+    }
+    bool moveValid()
+    {
+        foreach(UnityEngine.Transform child in transform)
+        {
+            //int x = Mathf.RoundToInt(child.transform.position.x);
+            int col = Mathf.RoundToInt(child.position.x);
+            int row = Mathf.RoundToInt(child.position.y);
+            if(row>=0&&row<height&&col>=0&&col<width)
+            {
+                if (bitmap[row, col] != null)
+                    return false;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    void selfDelete()
+    {
+        foreach (UnityEngine.Transform child in transform)
+        {
+            //int x = Mathf.RoundToInt(child.transform.position.x);
+            int col = Mathf.RoundToInt(child.position.x);
+            int row = Mathf.RoundToInt(child.position.y);
+            //Destroy(child.gameObject);
+            bitmap[row, col] = null;
+        }
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.DocZh.Components;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class start : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool gameover;
+    public Texture texture;
+    public Rect buttonarea;
+    public Rect gradearea;
+    public Rect overarea;
+    public int grade;
+    public GameObject[] toDelete;
+    public GameObject[] Blocks;
+    //public Texture play;
+    //public GUIContent mycontent;
+    public GUIStyle mystyle;
+    void Start()
+    {
+        gameover = true;
+        grade = 0;
+    }
+
+    void OnGUI()
+    {
+        //开始按钮
+        bool bt = GUI.Button(buttonarea,"Play");
+        //点击开始按钮，清理游戏界面，重新开始
+        if (bt)
+        {
+            UnityEngine.Debug.Log("button hit");
+            if (gameover)
+            {
+                grade = 0;
+                gameover = false;
+                toDelete = GameObject.FindGameObjectsWithTag("Blocks");
+                foreach(GameObject child in toDelete)
+                {
+                    child.SendMessage("selfDelete");
+                }
+                newAblock();
+            }
+        }
+        //分数显示
+        if(gameover)
+        {
+            GUI.Label(overarea, texture);
+        }
+        GUI.Label(gradearea,"得分："+grade,mystyle);
+
+    }
+    //产生一个新的Block
+    void addGrade()
+    {
+        ++grade;
+    }
+    void newAblock()
+    {
+        Instantiate(Blocks[Random.Range(0,Blocks.Length)],transform.position,Quaternion.identity);
+    }
+
+    void setGame()
+    {
+        gameover = true;
+    }
+
+}
